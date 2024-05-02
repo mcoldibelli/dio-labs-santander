ca770a223d8d40a4ada02164af13ef9b3f6fbe87
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-05-02 16:18:39 -0300

    feat: add challenge exceptions

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException() {}
+
+    public ParametrosInvalidosException(String message) {
+      super(message);
+    }
+}
